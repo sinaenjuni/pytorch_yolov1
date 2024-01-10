8406cdef81ea480540ee4959423a6c0182477ad9
sinaenjuni <y2k4388@gmail.com>
2024-01-10 18:09:59 +0900

    add test IoU test file

--- src/tests/iou_test.py
+++ src/tests/iou_test.py
@@ -18,14 +18,14 @@ if __name__ == "__main__":
     print(pred_bboxes)
     print(minmax2xywh(pred_bboxes))
 
-    # pred_bboxes = xywh2minmax(minmax2xywh(pred_bboxes))
+    pred_bboxes = xywh2minmax(minmax2xywh(pred_bboxes))
     print(pred_bboxes)
 
-    # img=torch.zeros((3, 300, 300), dtype=torch.uint8)
-    # img=draw_bounding_boxes(img, pred_bboxes, width=3, labels=['0','0'], colors=(255,255,0))
-    # img=draw_bounding_boxes(img, target_bboxes, width=3, labels=['1','1'], colors=(255,0,255))
-    # img = torchvision.transforms.ToPILImage()(img)
-    # img.show()
+    img=torch.zeros((3, 300, 300), dtype=torch.uint8)
+    img=draw_bounding_boxes(img, pred_bboxes, width=3, labels=['0','0'], colors=(255,255,0))
+    img=draw_bounding_boxes(img, target_bboxes, width=3, labels=['1','1'], colors=(255,0,255))
+    img = torchvision.transforms.ToPILImage()(img)
+    img.show()
 
-    # iou = intersection_over_union(pred_bboxes, target_bboxes)
-    # print(iou)
+    iou = intersection_over_union(pred_bboxes, target_bboxes)
+    print(iou)
